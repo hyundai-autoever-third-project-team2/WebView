86fe12a7a9477c78cb796db4be6c724a81add8c3
friend5hip <jwy97@naver.com>
2025-01-10 15:29:07 +0900

    desgin: 웹뷰 컴포넌트 클릭 시 나오는 파란색 하이라이트 제거

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,6 +6,11 @@ export const GlobalStyle = createGlobalStyle<{ type: 'mobile' | 'admin' }>`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    // 컴포넌트 클릭 시 파란색 하이라이트를 제거합니다.
+    -webkit-tap-highlight-color: transparent;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    user-select: none;
   }
 
   html, body {
