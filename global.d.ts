425983030e512d617fbfb7d48794bd1481c14a20
vinivin153 <vinivin153@naver.com>
2025-01-12 20:24:36 +0900

    feat: 카카오 인터페이스 추가

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,12 @@
-import { AndroidInterface } from 'types/android';
+import { AndroidInterface, KakaoInterface } from 'types/android';
 
 declare global {
   interface Window {
     Android: AndroidInterface;
+    Kakao: KakaoInterface;
     log: <T>(message: T) => void;
   }
   const log: <T>(message: T) => void;
   const Android: AndroidInterface;
+  const Kakao: KakaoInterface;
 }
--- src/types/android.ts
+++ src/types/android.ts
@@ -2,3 +2,7 @@ export interface AndroidInterface {
   showToast: (message: string) => void;
   openCamera: () => void;
 }
+
+export interface KakaoInterface {
+  kakaoLogin: () => void;
+}
