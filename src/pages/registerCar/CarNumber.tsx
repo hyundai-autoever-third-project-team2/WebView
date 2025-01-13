1d7fee2830ca7167f752144652659a3e22fc7fbf
vinivin153 <vinivin153@naver.com>
2025-01-13 19:14:29 +0900

    feat: 차량번호 입력 단계 페이지 구현

---
+++ src/pages/registerCar/CarNumber.tsx
@@ -0,0 +1,25 @@
+import Button from 'components/common/Button';
+import * as S from './RegisterCarPage.style';
+import { useState } from 'react';
+
+interface CarNumberProps {
+  handleNextClick: (data: string | File[]) => void;
+}
+
+export const CarNumber = ({ handleNextClick }: CarNumberProps) => {
+  const [carNumber, setCarNumber] = useState('');
+
+  const handleCarNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCarNumber(e.target.value);
+  };
+
+  return (
+    <S.CarNumberWrapper>
+      <S.Title>차량번호를 입력해주세요</S.Title>
+      <S.Input placeholder="123가1234" value={carNumber} onChange={handleCarNumberChange} />
+      <Button fixed onClick={() => handleNextClick(carNumber)}>
+        다음
+      </Button>
+    </S.CarNumberWrapper>
+  );
+};
