b61f3e8b3d13c12d109256fa27c903c755cb6869
vinivin153 <vinivin153@naver.com>
2025-01-13 19:15:53 +0900

    feat: 차량 판매 페이지 구현

---
+++ src/pages/registerCar/RegisterCarPage.style.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import { theme } from 'styles/theme';
+
+export const CarNumberWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+  padding-top: 84px;
+  gap: 1px;
+`;
+
+export const CarOutsieImagesWrapper = styled(CarNumberWrapper)``;
+
+export const CarInsideImagesWrapper = styled(CarNumberWrapper)``;
+
+export const CarIntroduceWrapper = styled(CarNumberWrapper)``;
+
+export const ImagesWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 140px);
+  grid-gap: 16px;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  margin-bottom: 16px;
+`;
+
+export const Title = styled.div`
+  color: ${theme.colors.neutral900};
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 20px;
+  letter-spacing: 0.25px;
+  margin-bottom: 8px;
+  align-self: flex-start;
+`;
+
+export const SubTitle = styled.div`
+  color: ${theme.colors.neutral500};
+  text-align: center;
+  font-family: 'Pretendard';
+  font-size: 14px;
+`;
+
+export const TextArea = styled.textarea`
+  width: 320px;
+  height: 140px;
+  flex-shrink: 0;
+  border-radius: 4px;
+  border: 1px solid #ff9d00;
+  background: #fff;
+  resize: none;
+  padding: 12px;
+  font-family: Pretendard;
+  font-size: 16px;
+
+  &:focus {
+    border: 2px solid ${theme.colors.primary};
+    outline: none;
+  }
+`;
+
+export const Input = styled.input`
+  width: 96%;
+  height: 53px;
+  padding: 10px 12px;
+  color: ${theme.colors.primary};
+  font-family: Pretendard;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 400;
+  border-radius: 9.464px;
+  border: 1px solid ${theme.colors.primary};
+
+  &::placeholder {
+    color: #ffdda6;
+  }
+
+  &:focus {
+    border: 2px solid ${theme.colors.primary};
+    outline: none;
+  }
+`;
