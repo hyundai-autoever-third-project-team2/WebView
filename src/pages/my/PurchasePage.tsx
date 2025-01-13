fc7636647d37593e4cc358bf90523ee5332ccac0
moaangel <126864067+moaangel@users.noreply.github.com>
2025-01-13 16:51:47 +0900

    feat: 구매내역 페이지 생성

---
+++ src/pages/my/PurchasePage.tsx
@@ -0,0 +1,14 @@
+import Toolbar from "components/common/Toolbar";
+
+function PurchasePage() {
+
+    return(
+        <>
+            <Toolbar showBackButton title="구매 내역"/>
+        </>
+    )
+
+
+}
+
+export default PurchasePage;
