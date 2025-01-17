eaa3ba7e68d6797e1d478d59d13ebf5a15cd62bf
vinivin153 <vinivin153@naver.com>
2025-01-17 16:35:11 +0900

    feat: 설문조사 api 구현

---
+++ src/api/survey/surveyApi.ts
@@ -0,0 +1,14 @@
+import { IServeyData } from 'types/survey';
+import { client } from 'utils/axiosInstance';
+
+export async function submitSurvey(surveyData: IServeyData | {}) {
+  try {
+    const response = await client.post('/user/survey', surveyData);
+    if (response.status === 200) {
+      console.log('설문제출 완료');
+    }
+  } catch (error) {
+    console.error('설문제출 실패', error);
+    throw error;
+  }
+}
